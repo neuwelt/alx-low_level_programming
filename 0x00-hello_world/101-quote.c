850df828423aa78cf70b4699b1deb7f7762cbe48
neuwelt <ineuwelt@neuwelt.co>
2022-04-07 23:08:12 +0000

    Program that prints exactly a string with a new line to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -9,6 +10,6 @@
 
 int main(void)
 {
-	fwrite(STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
+	write( STDOUT_FILENO, "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n", 59;
 	return (1);
 }
