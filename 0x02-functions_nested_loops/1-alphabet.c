f05030a1c731adcf042e8e005a00dcd8a3ab0b23
neuwelt <ineuwelt@neuwelt.co>
2022-04-13 07:09:09 +0000

    Correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main -check the code
- * Return: Always 0.
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
 	void print_alphabet(void)
