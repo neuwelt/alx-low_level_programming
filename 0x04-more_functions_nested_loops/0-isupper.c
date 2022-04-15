da822b8177a17b2301adadbfa4ffe48e23a62e4c
neuwelt <ineuwelt@neuwelt.co>
2022-04-15 00:19:15 +0000

    Write a function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check the code for Holberton School students.
+ * @c: Character input
+ * Return: Always 0.
+ */
+
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
