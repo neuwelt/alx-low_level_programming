876b050855ce6089b1769af2bc9868814aaec2da
neuwelt <ineuwelt@neuwelt.co>
2022-04-15 00:31:40 +0000

    README file correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,19 @@
 #include "main.h"
 
 /**
- * _isupper - check the code for Holberton School students.
- * @c: Character input
- * Return: Always 0.
+ * _isupper - checks for uppercase character
+ * @c: the character to print
+ *
+ * Return: 1 or 0
  */
-
-
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 65) && (c <= 90))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
