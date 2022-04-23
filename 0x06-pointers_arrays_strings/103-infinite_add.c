84e6f8592e7689084c921f477298469abd31409e
neuwelt <ineuwelt@neuwelt.co>
2022-04-23 03:31:59 +0000

    Correction

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,55 @@
 #include "main.h"
-#include <stdlib.h>
-#include <string.h>
-
 /**
- * infinite_add - Adds to numbers in a string
- * @n1: The number
- * @n2: The number
- * @r: The result
- * @size_r: The qty
+ * infinite_add -  adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: result lenght
+ * Return: sum
  *
- * Return: none
  */
+
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	char t;
-	int  i, j, k, size_1, size_2, a, b, c =  0;
+	/* local variable declaration */
+	int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-	size_1 = strlen(n1);
-	size_2 = strlen(n2);
-
-	if (size_1 > (size_r - 1) || size_2 > (size_r - 1))
+	while (n1[i] != '\0')
+		i++;
+	while (n2[j] != '\0')
+		j++;
+	if (i > j)
+		l = i;
+	else
+		l = j;
+	if (l + 1 > size_r)
 		return (0);
-
-	for (i = --size_1, j = --size_2, k = 0; i >= 0 || j >= 0; k++)
+	r[l] = '\0';
+	for (k = l - 1 ; k >= 0 ; k--)
 	{
+		i--;
+		j--;
 		if (i >= 0)
-			a = n1[i--] - 48;
+			f = n1[i] - '0';
 		else
-			a = 0;
+			f = 0;
 		if (j >= 0)
-			b = n2[j--] - 48;
+			s = n2[j] - '0';
 		else
-			b = 0;
-		r[k] = (a + b + c) % 10 + 48;
-		c = (a + b + c) / 10;
-		if (k >= (size_r - 2))
-			return (0);
+			s = 0;
+		r[k] = (f + s + d) % 10 + '0';
+		d = (f + s + d) / 10;
 	}
-	if (c > 0)
-		r[k++] = c + 48;
-	r[k] = '\0';
-	for (i = 0; i < (k / 2); i++)
+	if (d == 1)
 	{
-		t = r[i];
-		r[i] = r[k - i - 1];
-		r[k - i - 1] = t;
+		r[l + 1] = '\0';
+		if (l + 2 > size_r)
+			return (0);
+		while (l-- >= 0)
+			r[l + 1] = r[l];
+		r[0] = d + '0';
 	}
-
 	return (r);
 }
