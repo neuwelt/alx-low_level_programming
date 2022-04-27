eeced098d4e885848665a293bdd3ab0407aa645b
neuwelt <ineuwelt@neuwelt.co>
2022-04-27 00:13:50 +0000

    Correction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,13 +11,18 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+		int a;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == 'c')
-			return (s - 1);
-		else
-			return (NULL);
-	}
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
