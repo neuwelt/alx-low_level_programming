d604ac903dbc5742d8693b5bdbd1d41861e7fd11
neuwelt <ineuwelt@neuwelt.co>
2022-04-29 22:35:16 +0000

    Correction

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,7 +8,8 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') //\0 NULL Character/Terminator Marks Line End
+	/* '\0' NULL Character/Terminator Marks Line End */
+	if (*s == '\0')
 	{
 		_putchar("\n");
 		return;
