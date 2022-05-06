f0f435703859dc3d891617a3cff39c7e5321028f
neuwelt <ineuwelt@neuwelt.co>
2022-05-06 23:36:55 +0000

    Fix bug

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /**
  * malloc_checked - a function that allocates memory using malloc
@@ -10,12 +11,15 @@
  * Return: Returns value 98 on unsecessful termincation
  */
 
+
 void *malloc_checked(unsigned int b)
 {
 	int *ptr;
 
 	ptr = malloc(b);
 	if (ptr == NULL)
-		return(98);
+		exit(98);
+
+	return (ptr);
 
 }
