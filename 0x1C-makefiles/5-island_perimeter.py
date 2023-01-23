c095f6b60ed44fa7217e6b6412533068970885d5
neuwelt <ineuwelt@neuwelt.co>
2023-01-23 14:58:46 +0000

    Pushing Project code from un completed work

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,65 +1,28 @@
 #!/usr/bin/python3
-"""island_perimeter function"""
-
-
-def try_the_edges(r, c, grid):
-    """checks perimeter of individual cells for water (0)
-    Args:
-        r (int): row coordinate
-        c (int): column coordinate
-        grid (list): list of lists
-    Returns:
-        count of zeroes or sides cell is surrounded by water
-    """
-    count = 0
-    if r == 0 or grid[r - 1][c] == 0:
-        count += 1
-    if r == len(grid) - 1 or grid[r + 1][c] == 0:
-        count += 1
-    if c == 0 or grid[r][c - 1] == 0:
-        count += 1
-    if c == len(grid[0]) - 1 or grid[r][c + 1] == 0:
-        count += 1
-
-    return count
+"""Defines an island perimeter measuring function."""
 
 
 def island_perimeter(grid):
-    """perimeter of island (1) surrounded by water (0)
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
     Args:
-        grid (list): list of lists
+        grid (list): A list of list of integers representing an island.
     Returns:
-        perimeter number of cells the island is surrounded by water
+        The perimeter of the island defined in grid.
     """
-    perimeter = 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    if not grid:
-        return perimeter
-    if type(grid) is not list:
-        return perimeter
-    rows = len(grid)
-    if rows < 1:
-        return perimeter
-    columns = len(grid[0])
-
-    for r in range(rows):
-        for c in range(columns):
-            if grid[r][c] == 1:
-                perimeter += try_the_edges(r, c, grid)
-
-    return perimeter
-
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-
-    grid = [
-        [1, 1],
-        [1, 1]
-    ]
-    print(island_perimeter(grid))
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
